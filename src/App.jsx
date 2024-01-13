ffd96d797aefe6c7043d9cfe84ca6cff0db75ef5
Rldumdum <juelierldumdum@gmail.com>
2024-01-13 18:05:14 +0800

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import "./styles/outerCard.scss";
 import CardItem from "./components/CardItem";
+import { Shake } from "reshake";
+import Modal from "react-modal";
+import { motion } from "framer-motion";
 
-const images = [
+const easy_images = [
   { src: "https://picsum.photos/id/29/350/300", name: "Go Wild" },
   { src: "https://picsum.photos/id/30/350/300", name: "Shyamanata Baruah" },
   { src: "https://picsum.photos/id/31/350/300", name: "How-Soon-ngu" },
@@ -13,17 +16,74 @@ const images = [
   { src: "https://picsum.photos/id/35/350/300", name: "Shane Colella" },
   { src: "https://picsum.photos/id/36/350/300", name: "Vadim Sherbakov" },
 ];
+const hard_images = [
+  { src: "/photos_hard/ai-1.jpg" },
+  { src: "/photos_hard/ai-2.jpg" },
+  { src: "/photos_hard/ai-3.jpg" },
+  { src: "/photos_hard/ai-4.jpg" },
+  { src: "/photos_hard/ai-5.jpg" },
+  { src: "/photos_hard/ai-6.jpg" },
+  { src: "/photos_hard/ai-7.jpg" },
+  { src: "/photos_hard/ai-8.jpg" },
+  { src: "/photos_hard/ai-9.jpg" },
+  { src: "/photos_hard/ai-10.jpg" },
+  { src: "/photos_hard/ai-11.jpg" },
+  { src: "/photos_hard/ai-12.jpg" },
+  { src: "/photos_hard/ai-13.jpg" },
+];
+const hardcore_images = [
+  { src: "/photos_hardcore/Finn/ai-1.jpg" },
+  { src: "/photos_hardcore/Finn/ai-2.jpg" },
+  { src: "/photos_hardcore/Finn/ai-3.jpg" },
+  { src: "/photos_hardcore/Finn/ai-4.jpg" },
+  { src: "/photos_hardcore/Finn/ai-5.jpg" },
+  { src: "/photos_hardcore/Finn/ai-6.jpg" },
+  { src: "/photos_hardcore/Finn/ai-7.jpg" },
+  { src: "/photos_hardcore/Finn/ai-8.jpg" },
+  { src: "/photos_hardcore/Finn/ai-9.jpg" },
+  { src: "/photos_hardcore/Jake/ai-1.jpg" },
+  { src: "/photos_hardcore/Jake/ai-2.jpg" },
+  { src: "/photos_hardcore/Jake/ai-3.jpg" },
+  { src: "/photos_hardcore/Jake/ai-4.jpg" },
+  { src: "/photos_hardcore/Jake/ai-5.jpg" },
+  { src: "/photos_hardcore/Jake/ai-6.jpg" },
+  { src: "/photos_hardcore/Jake/ai-7.jpg" },
+  { src: "/photos_hardcore/Jake/ai-8.jpg" },
+  { src: "/photos_hardcore/Jake/ai-9.jpg" },
+];
 
 function App() {
+  const maxscore =
+    easy_images.length + hard_images.length + hardcore_images.length;
   const [cards, setCards] = useState([]);
   const [gameStarted, setGameStarted] = useState(false);
   const [cardChoice, setCardChoice] = useState([]);
   const [score, setScore] = useState(-2);
   const [bestScore, setBestScore] = useState(0);
-  const shuffleCards = () => {
-    const shuffledCards = [...images]
+  const [difficulty, setDifficulty] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [h, setH] = useState(0);
+  const [scale, setScale] = useState(0);
+  let imageToShuffle;
+  const shuffleCards = (version) => {
+    switch (version) {
+      case "easy":
+        setDifficulty("Easy");
+        imageToShuffle = easy_images;
+        break;
+      case "hard":
+        setDifficulty("Hard");
+        imageToShuffle = hard_images;
+        break;
+      case "hardcore":
+        setDifficulty("Hardcore");
+        imageToShuffle = hardcore_images;
+        break;
+    }
+    const shuffledCards = [...imageToShuffle]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
+    console.log("card has been shuffled");
 
     setCards(shuffledCards);
     setGameStarted(true);
@@ -37,7 +97,7 @@ function App() {
   });
 
   const isDuplicate = checkvalue.some((item, index) => {
-    console.log('item', item)
+    console.log("item", item);
     return checkvalue.indexOf(item) != index;
   });
 
@@ -45,6 +105,9 @@ function App() {
     if (isDuplicate) {
       setCardChoice([]);
       setScore(-1);
+      setH(200);
+
+      // cleanUp
     } else {
       setScore((prevScore) => prevScore + 1);
     }
@@ -56,13 +119,121 @@ function App() {
       setBestScore(score);
     }
 
+    if (difficulty === "Easy" && score === easy_images.length) {
+      setModalIsOpen(true);
+      setScale(1);
+      setTimeout(() => {
+        setScale(0);
+      }, 2000);
+      setTimeout(() => {
+        setModalIsOpen(false);
+      }, 3000);
+      shuffleCards("hard");
+    } else if (
+      difficulty === "Hard" &&
+      score === hard_images.length + easy_images.length
+    ) {
+      setModalIsOpen(true);
+      setScale(1);
+      setTimeout(() => {
+        setScale(0);
+      }, 2000);
+      setTimeout(() => {
+        setModalIsOpen(false);
+      }, 3000);
+      shuffleCards("hardcore");
+    } else if (difficulty === "Hardcore" && score === maxscore) {
+      alert("You won the game");
+      setGameStarted(false);
+    }
+
     cards.sort(() => Math.random() - 0.6);
 
     return () => {};
   }, [score, bestScore, cards]);
 
+  useEffect(() => {
+    if (h !== 0) {
+      setTimeout(() => {
+        setH((prevH) => prevH - 10);
+      }, 10);
+    }
+  }, [h]);
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      borderRadius: "20px",
+      height: "420px",
+    },
+  };
+
   return (
     <div>
+      {gameStarted && (
+        <>
+          <img
+            style={{
+              position: "absolute",
+              left: "100px",
+              height: "50px",
+              width: "50px",
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              setScore(0);
+              setGameStarted(false);
+            }}
+            src="/back-icon.svg"
+          ></img>
+          <h3 style={{ position: "absolute", right: "100px" }}>
+            Max Score: {maxscore}
+          </h3>
+        </>
+      )}
+
+      <Modal
+        isOpen={modalIsOpen}
+        style={customStyles}
+        contentLabel="EXAMPLE MODAL"
+      >
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: scale }}
+          transition={{ duration: 2 }}
+        >
+          <h1>Congratulations!</h1>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <h1>
+              Difficulty:
+              <p
+                style={{
+                  color:
+                    difficulty === "Easy"
+                      ? "green"
+                      : difficulty === "Hard"
+                      ? "orange"
+                      : "red",
+                }}
+              >
+                {difficulty}
+              </p>
+            </h1>
+          </div>
+        </motion.div>
+      </Modal>
       <h1>Memory game</h1>
       <p>
         Instruction: Test your memory skills by clicking on different images
@@ -70,16 +241,57 @@ function App() {
       </p>
       {gameStarted && (
         <div>
+          <p
+            style={{
+              color:
+                difficulty === "Easy"
+                  ? "green"
+                  : difficulty === "Hard"
+                  ? "orange"
+                  : "red",
+            }}
+          >
+            Difficulty: {difficulty}
+          </p>
           <p>Score: {score}</p>
           <p>Best Score: {bestScore}</p>
         </div>
       )}
-      {!gameStarted && <button onClick={shuffleCards}>Start Game</button>}
+      {!gameStarted && (
+        <>
+          <h2>Select your level of difficulty: </h2>
+          <div
+            style={{ display: "flex", flexDirection: "column", gap: "20px" }}
+          >
+            <button onClick={() => shuffleCards("easy")}>Easy</button>
+            <button onClick={() => shuffleCards("hard")}>Hard</button>
+            <button onClick={() => shuffleCards("hardcore")}>Hardcore</button>
+          </div>
+        </>
+      )}
       {gameStarted && (
         <div className="outerCard">
           {cards.map((card) => {
             return (
-              <CardItem key={card.id} card={card} handleChoice={handleChoice} />
+              <Shake
+                h={h}
+                v={0}
+                r={0}
+                dur={560}
+                int={18}
+                max={100}
+                fixed={true}
+                fixedStop={false}
+                freez={true}
+                key={card.id}
+              >
+                <CardItem
+                  key={card.id}
+                  card={card}
+                  handleChoice={handleChoice}
+                  h={h}
+                />
+              </Shake>
             );
           })}
         </div>
--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,12 +1,16 @@
 import "../styles/card.scss";
 import PropTypes from "prop-types";
 
-const CardItem = ({ card, handleChoice }) => {
+const CardItem = ({ card, handleChoice, h }) => {
   const handleClick = () => {
     handleChoice(card);
   };
   return (
-    <div className="card" onClick={handleClick}>
+    <div
+      className="card"
+      onClick={handleClick}
+      style={{ backgroundColor: h === 0 ? "#f4f4f4" : "#8B0000" }}
+    >
       <div className="card-img">
         <img src={card.src} className="card-img"></img>
       </div>
@@ -18,6 +22,7 @@ CardItem.propTypes = {
   card: PropTypes.shape({
     src: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
+    h: PropTypes.number.isRequired,
     // Add other properties as needed
   }).isRequired,
   handleChoice: PropTypes.func.isRequired,
